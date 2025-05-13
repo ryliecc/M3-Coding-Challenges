611d88a793641e07325d01ccfc58e73ebb3d4343
Rylie Castell <rylie.castell@gmail.com>
2025-05-13 10:05:00 +0200

    Initial Commit

---
+++ CodingChallengesM3/CodingChallengesM3App.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallengesM3App.swift
+//  CodingChallengesM3
+//
+//  Created by Rylie Castell on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallengesM3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodingChallengesM3Tests/CodingChallengesM3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallengesM3Tests.swift
+//  CodingChallengesM3Tests
+//
+//  Created by Rylie Castell on 13.05.25.
+//
+
+import Testing
+@testable import CodingChallengesM3
+
+struct CodingChallengesM3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
