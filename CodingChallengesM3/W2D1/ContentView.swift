525f7a4e76c8441abf0771dff43fde0ab08cfb07
Rylie Castell <rylie.castell@gmail.com>
2025-05-19 13:10:08 +0200

    add coding challenge 2 week 2 day 1

--- CodingChallengesM3/W2D1/ContentView.swift
+++ CodingChallengesM3/W2D1/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var number: Int = 10
+    @State var number: Int = 10 {
+        didSet {
+            if number == 0 && oldValue >= 1 {
+                fireworkIsVisible = true
+            }
+        }
+    }
+    @State var fireworkIsVisible: Bool = false
     var body: some View {
         CountdownView(number: $number)
         Button("Weiter") {
             number -= 1
         }
+        Text(fireworkIsVisible ? "🎆" : "🌌")
+            .font(.system(size: 200))
+        Toggle("Zeige Feuerwerk an", isOn: $fireworkIsVisible)
+            .padding()
     }
 }
 
